53d17990266517b7de00ebb94ecf0f4ee098ff16
ongphil <35523052+ongphil@users.noreply.github.com>
2018-12-23 18:30:33 +0100

    SplashActivity fait, ajout des activités Login et Main

--- app/src/main/java/justkidding/justkidding/SplashActivity.java
+++ app/src/main/java/justkidding/justkidding/SplashActivity.java
@@ -1,13 +1,29 @@
 package justkidding.justkidding;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
